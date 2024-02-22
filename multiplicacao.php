018346809b4bdb7c7e07058b0f01314e82b9a394
Rebeca Sch <134728551+Rebs-Sch@users.noreply.github.com>
2024-02-22 09:43:01 -0300

    Create multiplicacao.php
    Criei a atividade aí.

---
+++ multiplicacao.php
@@ -0,0 +1,8 @@
+<?php
+
+$multiplicando = readline ("Qual é o número a ser multiplicado?")
+$multiplicador = readline ("Que número o multiplicará?")
+
+for ($i = 0; $i < $multiplicador; $i++){
+  $multiplicando = $multiplicando + $multiplicando
+}
